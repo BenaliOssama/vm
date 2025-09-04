99bc250e68df9eea751cd094b58e6b06910a9bd1
obenali <oussama.benali@ump.ac.ma>
2025-09-04 19:48:27 +0100

    add downloaded file form the discreption of the project that has assembler and vm and some players in .s format that can be assembled into .cor files and run by the vm, also add a python script to read the .cor file for analysis and reverse engeneering

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,12 @@
 use vm::*;
 
 pub fn parse_arguments(args: Vec<String>) -> Vec<String> {
-    if args.len() < 3 {
+    if args.len() < 2 {
         panic!("{}", red("not enought argumentsh"));
     }
     //TODO!
+    // make sure the file ends with the right extention .cor
+    // for now i guess only one player is good. 
     return args[1..].to_vec();
 }
 
