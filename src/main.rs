d1f6dec88fdb98d9d0a6258bef73be3545fcbb7c
OUSSAMABENALI <oussama.benali@ump.ac.ma>
2025-09-10 02:09:16 +0100

    move the logic of parsing to the utils module

--- src/main.rs
+++ src/main.rs
@@ -16,76 +16,15 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     // get players in some vector of players
-    let buffer = parse_arguments(args);
+    let player = parse_arguments(args);
 
-    let mut prev = 0;
-    let mut next = 4;
-
-    let magic = &buffer[prev..next];
-    // 128 + 4
-    prev = next;
-    next = next + 128;
-
-    let name = std::str::from_utf8(&buffer[prev..next]).unwrap(); //.trim();
-    let name: String = name.chars().filter(|&c| c != '\0').collect();
-
-    // Print as uppercase hex
-    for b in magic {
-        print!("{:02x} ", b);
-    }
-
-    prev = next + 4; // skip 4 bytes 
-    next = prev + 4;
-
-    let mut arr = [0u8; 4];
-    println!("this is what i read: {:?}", &buffer[prev..next]);
-    arr.copy_from_slice(&buffer[prev..next]);
-    println!("\nBytes for size: {:02x?}", arr);
-    let size = u32::from_be_bytes(arr);
-
-    prev = next; // skip 4 bytes 
-    next = prev + 2048;
-
-    let disc = std::str::from_utf8(&buffer[prev..next]).unwrap().trim();
-    let disc: String = disc.chars().filter(|&c| c != '\0').collect();
-    // Print as uppercase hex
-    for b in magic {
-        print!("{:02x} ", b);
-    }
-
-    prev = next + 4; // skip 4 bytes 
-    next = prev + size as usize;
-
-    let program = &buffer[prev..next];
-
-    println!(" -> should be -> {}", "00 ea 83 f3");
-    println!("name -> {}", name);
-
-    println!("size {:}", size);
-    println!("description {:}", disc);
-    for b in program {
-        print!("{:02x} ", b);
-    }
-    println!();
-
-    println!("the name size is {}", name.len());
-    let player = Player::new(
-        1,
-        name.to_string(),
-        disc.to_string(),
-        program.to_vec(),
-        size,
-        0,
-    );
-
-    println!("{player}");
     let mut arena = Arena::new();
 
-    arena.write(0, program);
-    //println!("fulll arena: -> : {:?}", arena);
+    arena.write(0, &player.code);
 
     let mut process1 = Process::new();
     process1.execute_cycle(&mut arena);
+
     // // make new vm
     // let mut vm = VirtualMachine::new();
 
--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,10 @@
+use crate::parser::Player;
 use std::fs::File;
 use std::io::{self, Read};
 use vm::*;
 
-pub fn parse_arguments(args: Vec<String>) -> Vec<u8> {
+pub fn parse_arguments(args: Vec<String>) -> Player {
+    /*_____________read arguments___________________ */
     if args.len() != 2 {
         panic!("{}", red("not enought argumentsh!"));
     }
@@ -21,5 +23,46 @@ pub fn parse_arguments(args: Vec<String>) -> Vec<u8> {
 
     file.read_to_end(&mut buffer).unwrap();
 
-    return buffer;
+    let mut prev = 0;
+    let mut next = 4;
+
+    let magic = &buffer[prev..next];
+    // 128 + 4
+    prev = next;
+    next = next + 128;
+
+    let name = std::str::from_utf8(&buffer[prev..next]).unwrap(); //.trim();
+    let name: String = name.chars().filter(|&c| c != '\0').collect();
+
+
+    prev = next + 4; // skip 4 bytes 
+    next = prev + 4;
+
+    let mut arr = [0u8; 4];
+    arr.copy_from_slice(&buffer[prev..next]);
+    let size = u32::from_be_bytes(arr);
+
+    prev = next; // skip 4 bytes 
+    next = prev + 2048;
+
+    let disc = std::str::from_utf8(&buffer[prev..next]).unwrap().trim();
+    let disc: String = disc.chars().filter(|&c| c != '\0').collect();
+
+    prev = next + 4; // skip 4 bytes 
+    next = prev + size as usize;
+
+    let program = &buffer[prev..next];
+
+
+    let player = Player::new(
+        1,
+        name.to_string(),
+        disc.to_string(),
+        program.to_vec(),
+        size,
+        0,
+    );
+
+    println!("{player}");
+    return player;
 }
