93f74eec62a054431febea894b6a792af8636a67
OUSSAMABENALI <oussama.benali@ump.ac.ma>
2025-09-10 01:30:54 +0100

    add a table to the library that help print a table in a good format and implement display for the player not tested yet

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 use std::fmt::Formatter;
-
+use vm::*;
 
 pub struct Player {
     pub id: i32, // The player id (assigned by the VM, starting from 1)
@@ -32,7 +32,23 @@ impl Player {
 }
 
 impl Display for Player {
-    fn fmt(&self, f : &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
-        todo!()
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        // make a table
+        let mut table = Table::new();
+        // add headers id, name, comment
+        table.add_header("id");
+        table.add_header("name");
+        table.add_header("comment");
+        // add rows
+        table.add_row(&vec![
+            self.id.to_string(),
+            self.name.clone(),
+            self.comment.clone(),
+        ]);
+        println!("{table}");
+        // make a table
+        // add headers code , size start_address,
+        // add rows
+        Ok(())
     }
 }
