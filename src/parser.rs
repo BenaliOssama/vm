4b4611805ef890153c395684b8ab1453adc91a0b
OUSSAMABENALI <oussama.benali@ump.ac.ma>
2025-09-10 01:13:18 +0100

    work on the player module andd a constructor and prototyping for making a display method for the player type that shows the player nicely

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,7 @@
+use std::fmt::Display;
+use std::fmt::Formatter;
+
+
 pub struct Player {
     pub id: i32, // The player id (assigned by the VM, starting from 1)
     pub name: String,
@@ -7,9 +11,28 @@ pub struct Player {
     pub start_address: usize, // Where in the arena the player's code is loaded
 }
 
-
 impl Player {
-    pub fn new(){
+    pub fn new(
+        id: i32,
+        name: String,
+        comment: String,
+        code: Vec<u8>,
+        size: u32,
+        start_address: usize,
+    ) -> Self {
+        Self {
+            id,
+            name,
+            comment,
+            code,
+            size,
+            start_address,
+        }
+    }
+}
+
+impl Display for Player {
+    fn fmt(&self, f : &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
         todo!()
     }
 }
