d60b37812fde78bbfe9749a2491f821eb4a1f6dd
OUSSAMABENALI <oussama.benali@ump.ac.ma>
2025-12-13 15:26:16 +0100

    add test for add

--- tests/vm_basic.rs
+++ tests/vm_basic.rs
@@ -12,8 +12,8 @@ this test should cover all instructions and make sure they do the job as should
 - [X] ld — load a value into a register
 - [X] st — store a register value into another register or memory
 - [X] add — add two registers and store the result
-- [ ] sub — subtract two registers and store the result
-- [ ] and — bitwise AND operation
+- [X] sub — subtract two registers and store the result
+- [X] and — bitwise AND operation
 - [ ] or — bitwise OR operation
 - [ ] xor — bitwise XOR operation
 - [z] zjmp — conditional jump if carry is set
@@ -292,6 +292,68 @@ fn test_4() {
         vm.cycle();
     }
     assert_eq!(vm.processes[0].registers[4 - 1], -1);
+    // ld
+    for _ in 0..5 {
+        for process in &mut vm.processes {
+            if process.state() == State::NoInstruction {
+                process.fetch_decode(&mut vm.arena);
+            }
+        }
+        vm.cycle();
+    }
+    //ld 5 zjmp 20
+    for _ in 0..20 {
+        for process in &mut vm.processes {
+            if process.state() == State::NoInstruction {
+                process.fetch_decode(&mut vm.arena);
+            }
+        }
+        vm.cycle();
+    }
+    assert_eq!(vm.processes[0].registers[2 - 1], 0);
+    assert_eq!(vm.processes[0].pc.get(), 0);
+}
+#[test]
+fn test_and() {
+    // This simulates what main() does
+    let args = vec![
+        "vm".into(),
+        "playground/players_src/pierino_and_ind_ind.cor".into(),
+    ];
+    let player = parse_arguments(args).expect("parse failed");
+
+    let arena = Arena::new();
+    let process = Process::new(player.id, 0);
+
+    println!("{player}");
+    println!("{}", process);
+    //println!("{}", arena);
+
+    let mut vm = VirtualMachine::create(arena.clone(), vec![process]);
+
+    vm.load_player(player);
+    // live 10
+    for _ in 0..10 {
+        for process in &mut vm.processes {
+            if process.state() == State::NoInstruction {
+                process.fetch_decode(&mut vm.arena);
+            }
+        }
+        vm.cycle();
+    }
+    assert_eq!(vm.processes[0].live_status.executed, true);
+    assert_eq!(vm.processes[0].live_status.player_id, -1);
+    assert_eq!(vm.processes[0].live_status.nbr_live, 1);
+    //ld 6
+    for _ in 0..6 {
+        for process in &mut vm.processes {
+            if process.state() == State::NoInstruction {
+                process.fetch_decode(&mut vm.arena);
+            }
+        }
+        vm.cycle();
+    }
+    assert_eq!(vm.processes[0].registers[3 - 1], 770);
     // ld
     for _ in 0..5 {
         for process in &mut vm.processes {
