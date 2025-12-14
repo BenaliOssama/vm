01b62c4ec9fe586d190648bf26e1add62a39737b
fihry <fihrylara@gmail.com>
2025-12-14 03:33:09 +0100

    add the handling of corrupted files

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,4 @@
 use crate::player::Player;
-use crate::vm::*;
 use crate::*;
 use std::fs::File;
 use std::io::Read;
@@ -16,21 +15,32 @@ pub fn parse_arguments(args: Vec<String>) -> Result<Player, String> {
         panic!("{}", red("bad file extention!"));
     }
 
-    let mut file = File::open(file_name).unwrap();
+    let mut file = File::open(file_name)
+            .map_err(|e| red(&format!("Error Opening the file, {e}")))?;
 
     let mut buffer = Vec::new();
 
-    file.read_to_end(&mut buffer).unwrap();
+    file.read_to_end(&mut buffer)
+            .map_err(|e| red(&format!("Error reading the file, {e}")))?;
+
+    if buffer.len() < config::HEADERS_SIZE {
+        return Err(red("the file are too smaaaaal"));
+    }
 
     let mut prev = 0;
     let mut next = 4;
 
-    let _magic = &buffer[prev..next];
+    let magic = &buffer[prev..next];
+    if magic != [0x00, 0xea, 0x83, 0xf3] {
+        println!("invalid magic number: {:?}", magic);
+        return Err(format!("invalid magic number: {:?}", magic));
+    }
     // 128 + 4
     prev = next;
     next = next + 128;
 
-    let name = std::str::from_utf8(&buffer[prev..next]).unwrap(); //.trim();
+    let name = std::str::from_utf8(&buffer[prev..next])
+                .map_err(|e| red(&format!("small file {e}")))?;
     let name: String = name.chars().filter(|&c| c != '\0').collect();
 
     prev = next + 4; // skip 4 bytes 
@@ -39,18 +49,23 @@ pub fn parse_arguments(args: Vec<String>) -> Result<Player, String> {
     let mut arr = [0u8; 4];
     arr.copy_from_slice(&buffer[prev..next]);
     let size = u32::from_be_bytes(arr);
-
+    if size as usize > config::PLAYER_MAX_SIZE {
+        return Err(red("the file size are too big"));
+    }
     prev = next; // skip 4 bytes 
     next = prev + 2048;
 
-    let disc = std::str::from_utf8(&buffer[prev..next]).unwrap().trim();
+    let disc = std::str::from_utf8(&buffer[prev..next])
+                                    .map_err(|e| red(&format!("small file {e}")))?.trim();
     let disc: String = disc.chars().filter(|&c| c != '\0').collect();
 
     prev = next + 4; // skip 4 bytes 
     next = prev + size as usize;
 
     let program = &buffer[prev..next];
-
+    if program.len() != size as usize {
+        return Err(red("the size is the header not the actual program size "));
+    }
     let player = Player::new(
         -1,
         name.to_string(),
@@ -62,3 +77,4 @@ pub fn parse_arguments(args: Vec<String>) -> Result<Player, String> {
 
     return Ok(player);
 }
+
