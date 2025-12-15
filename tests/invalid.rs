83fbe74e5174082a3962557909d1e6b61380a98f
OUSSAMABENALI <oussama.benali@ump.ac.ma>
2025-12-15 19:15:04 +0100

    add a file for tests that should fail

---
+++ tests/invalid.rs
@@ -0,0 +1,44 @@
+use vm::State;
+use vm::*;
+
+/*(not working, these are not valid players, they should be not valid) */
+#[test]
+fn instr_not_exist() {
+    todo!()
+}
+
+#[test]
+fn not_existing_label() {
+    todo!()
+}
+
+#[test]
+fn register_too_big() {
+    todo!()
+}
+
+#[test]
+fn repated_label() {
+    todo!()
+}
+
+#[test]
+fn invalid_extension_k() {
+    todo!()
+}
+
+#[test]
+fn number_too_big() {
+    todo!()
+}
+
+#[test]
+fn register_too_small() {
+    todo!()
+}
+/* (special r out of range) */
+
+#[test]
+fn r_out_of_range() {
+    todo!()
+}
